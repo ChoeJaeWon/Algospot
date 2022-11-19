6d21525b318026e7b344194913268cca644663c1
JaeWon Choe <dnjsdlghdi@gmail.com>
2022-11-19 11:16:52 +0900

    template

---
+++ templete.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <memory.h>
+#include <algorithm>
+
+using namespace std;
+
+const int INF = 987654321;
+int C;
+
+void input() {
+
+}
+
+void init() {
+
+}
+
+void setup() {
+
+}
+
+void solve() {
+
+}
+
+int main() {
+    scanf("%d", &C);
+    while (C--) {
+        init();
+        input();
+        setup();
+        solve();
+
+    }
+}
